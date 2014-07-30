5fe9332e20e74ad5d17751d352c198f0b4985219
Mahmut Gundogdu <mahmutgundogdu@hotmail.com.tr>
2014-07-30 22:26:53 +0300

    Test edilmiş ilk çalışır kopya
    Android 4.4 üzerinde test edildi. 4.0.3 den 4.4 kadar sürümlerde
    çalışması lazım.

---
+++ DugunBugunMu/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.Webkit;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace DugunBugunMu
+{
+	[Activity (Label = "Düğün bu gün mü?", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		 
+		WebView web_view;
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			web_view = FindViewById<WebView> (Resource.Id.webview);
+			web_view.Settings.JavaScriptEnabled = true;
+			web_view.LoadUrl ("http://bugunmahmutundugunumu.info");
+			 
+		}
+	}
+}
+
+
